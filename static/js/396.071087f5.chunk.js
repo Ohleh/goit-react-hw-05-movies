fe6e5b755f696542a37a18344c4d0cd902a186cc
Ohleh <94701344+Ohleh@users.noreply.github.com>
2022-08-16 17:07:24 +0000

    Deploying to gh-pages from @ Ohleh/goit-react-hw-05-movies@48341656cc5efdebd2de1fbb2765b404ebb95dcd 🚀

---
+++ static/js/396.071087f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[396],{8396:function(e,r,t){t.r(r);var o=t(501),c=t(6871),n=t(184);r.default=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)(o.OL,{to:"/",style:function(e){return e.isActive?{color:"red"}:{color:"blue"}},children:"Home"}),(0,n.jsx)(o.OL,{to:"/movies",style:function(e){return e.isActive?{color:"red"}:{color:"blue"}},children:"Movies"}),(0,n.jsx)("hr",{}),(0,n.jsx)("main",{children:(0,n.jsx)(c.j3,{})})]})}}}]);
+//# sourceMappingURL=396.071087f5.chunk.js.map
